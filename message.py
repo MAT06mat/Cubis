a2a01b084bf6828635d3b70369480f85c92f1210
Matthieu <matthieu.felten22@gmail.com>
2023-06-28 16:00:13 +0200

    add reset and quit button in menu in game

--- message.py
+++ message.py
@@ -80,15 +80,44 @@ class PlayMessage(RelativeLayout):
         self.parent.message_pop()
 
 
+class SettingButton(Button):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Clock.schedule_interval(self.loop, 1/60)
+    
+    def loop(self, *args):
+        self.width = self.parent.height/3
+        self.height = self.width
+        self.y = self.parent.height/6
+
+
+class ResetButton(Button):
+    def __init__(self, id_level, **kwargs):
+        super().__init__(**kwargs)
+        self.id_level = id_level
+        Clock.schedule_interval(self.loop, 1/60)
+    
+    def loop(self, *args):
+        self.width = self.parent.height/3
+        self.height = self.width
+        self.y = self.parent.setting_button.y
+        self.x = self.parent.setting_button.x - self.width*0.7
+
+    def on_press(self):
+        Window.app.manager.start_level(self.id_level)
+        return super().on_press()
+
+
 class QuitButton(Button):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         Clock.schedule_interval(self.loop, 1/60)
     
     def loop(self, *args):
-        self.width = self.parent.width/4
-        self.height = self.width/8*5
-        self.y = self.parent.height/6
+        self.width = self.parent.height/3
+        self.height = self.width
+        self.y = self.parent.setting_button.y
+        self.x = self.parent.setting_button.x + self.width*0.7
 
 
 class MenuMessage(RelativeLayout):
@@ -98,11 +127,20 @@ class MenuMessage(RelativeLayout):
         self.size_hint = (1, None)
         self.id_level = id_level
         self.mode = mode
-        self.add_widget(Cadre())
-        self.add_widget(Back())
-        self.add_widget(ModeLabel(mode=mode))
-        self.add_widget(LevelName(text_var=self.id_level))
-        self.add_widget(QuitButton())
+        self.cadre = Cadre()
+        self.add_widget(self.cadre)
+        self.back = Back()
+        self.add_widget(self.back)
+        self.mode_label = ModeLabel(mode=mode)
+        self.add_widget(self.mode_label)
+        self.level_name = LevelName(text_var=self.id_level)
+        self.add_widget(self.level_name)
+        self.quit_button = QuitButton()
+        self.add_widget(self.quit_button)
+        self.setting_button = SettingButton()
+        self.add_widget(self.setting_button)
+        self.reset_button = ResetButton(id_level=self.id_level)
+        self.add_widget(self.reset_button)
         Clock.schedule_interval(self.loop, 1/60)
     
     def loop(self, *args):
