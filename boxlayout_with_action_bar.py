c01744149a81ce8669f8996d74597dfe7708b14e
Matthieu <matthieu.felten22@gmail.com>
2023-07-11 13:27:45 +0200

    Amélioration du code

--- boxlayout_with_action_bar.py
+++ boxlayout_with_action_bar.py
@@ -1,7 +1,6 @@
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.actionbar import ActionPrevious
 
 Builder.load_file("boxlayout_with_action_bar.kv")
 
