b21d7131178603039eb59d1f143c1fff59e92b72
Matthieu <matthieu.felten22@gmail.com>
2023-07-11 13:26:23 +0200

    Amélioration du code

--- navigation_sreen_manager.py
+++ navigation_sreen_manager.py
@@ -17,33 +17,28 @@ class TransitionScreen(Screen):
 class NavigationScreenManager(ScreenManager):
     screen_stack = []
     level = None
-    
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.id = 0
-        self.transition = FadeTransition(duration=0.2)
+    id = 0
+    transition = FadeTransition(duration=0.2)
 
+    def change_transition(self, transition_screen, screen_name):
+        if transition_screen:
+            self.transition = FadeTransition(duration=0.4)
+            self.current = "TransitionScreen"
+        else:
+            self.transition = FadeTransition(duration=0)
+            self.current = screen_name
+    
     def push(self, screen_name, transition_screen=True):
         if screen_name not in self.screen_stack:
             self.screen_stack.append(self.current)
-            if transition_screen:
-                self.transition = FadeTransition(duration=0.4)
-                self.current = "TransitionScreen"
-            else:
-                self.transition = FadeTransition(duration=0)
-                self.current = screen_name
+            self.change_transition(transition_screen, screen_name)
             self.next_current = screen_name
 
     def pop(self, transition_screen=True):
         if len(self.screen_stack) > 0:
             screen_name = self.screen_stack[-1]
             del self.screen_stack[-1]
-            if transition_screen:
-                self.transition = FadeTransition(duration=0.4)
-                self.current = "TransitionScreen"
-            else:
-                self.transition = FadeTransition(duration=0)
-                self.current = screen_name
+            self.change_transition(transition_screen, screen_name)
             self.next_current = screen_name
     
     def start_level(self, id_level=0):
@@ -51,27 +46,20 @@ class NavigationScreenManager(ScreenManager):
         self.last_level = None
         if self.level != None:
             self.last_level = self.level
+        name = "Level"+str(self.id)
         if id_level == 0:
-            self.level = InfiniteGame(name="Level"+str(self.id))
+            self.level = InfiniteGame(name=name)
         else:
-            self.level = StoryGame(name="Level"+str(self.id), id_level=id_level)
+            self.level = StoryGame(name=name, id_level=id_level)
         self.add_widget(self.level)
-        self.push("Level"+str(self.id))
+        self.push(name)
         if self.last_level != None:
             self.remove_widget(self.last_level)
             self.screen_stack.remove(self.last_level.name)
     
     def quit_level(self, transition_screen=True):
         if len(self.screen_stack) > 0:
-            screen_name = self.screen_stack[-1]
-            del self.screen_stack[-1]
-            if transition_screen:
-                self.transition = FadeTransition(duration=0.4)
-                self.current = "TransitionScreen"
-            else:
-                self.transition = FadeTransition(duration=0)
-                self.current = screen_name
-            self.next_current = screen_name
+            self.pop(transition_screen)
             try:
                 test = self.level.id_level
             except AttributeError:
