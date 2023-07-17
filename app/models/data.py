41fd33c4cf2b1530ba0bc2699d9ae56724ee1faf
Matthieu <matthieu.felten22@gmail.com>
2023-07-17 15:53:07 +0200

    Modification des chemins d'accès

--- app/models/data.py
+++ app/models/data.py
@@ -3,12 +3,12 @@ import os
 
 
 class Data:
-    path = "data/"
+    path = "data"
     path_rel = None
     
     def get(self, key: str):
         key = str(key)
-        with open(self.path+self.path_rel) as file:
+        with open(self.get_path()) as file:
             data = json.load(file)
         if key == 'all':
             return data
@@ -24,13 +24,23 @@ class Data:
             data = element
         else:
             try:
-                with open(self.path+self.path_rel) as file:
+                with open(self.get_path()) as file:
                     data = json.load(file)
                 data[key] = element
             except KeyError:
                 return KeyError
-        with open(self.path+self.path_rel, "w") as file:
+        with open(self.get_path(), "w") as file:
             file.write(json.dumps(data))
+    
+    def get_path(self):
+        # Obtenez le chemin absolu du répertoire "app/models"
+        models_dir = os.path.dirname(os.path.abspath(__file__))
+        # Obtenez le chemin absolu du répertoire "app"
+        app_dir = os.path.dirname(models_dir)
+        # Obtenez le chemin absolu du répertoire parent de "app" qui est le répertoire principal du projet
+        main_dir = os.path.dirname(app_dir)
+        path = os.path.join(main_dir, self.path, self.path_rel)
+        return path
 
 
 class SettingsData(Data):
