4fc3150cf691c09a3c23bbc6025d989d4a296fce
Matthieu <matthieu.felten22@gmail.com>
2024-01-01 11:05:31 +0100

    Remove decorators

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,18 +1,16 @@
 """
 # Import files:
-    - decorators.py
     - display_grid.py
     - loop.py
     - message.py
 """
 
-from models.decorators import if_no_message, if_no_piece
 from models.display_grid import COLOR, ANIMATION_LIST, DisplayGrid
 from models.grid_calculation import GridCalculation, generate_grid, turn, symmetry, random_grid
 from models.loop import Loop
 from models.message import PlayMessage, MenuMessage, VictoireMessage, InfoMessage
 
-__all__ = (if_no_message.__name__, if_no_piece.__name__, DisplayGrid.__name__, 
+__all__ = (DisplayGrid.__name__, 
            GridCalculation.__name__, generate_grid.__name__, turn.__name__,
            symmetry.__name__, random_grid.__name__, Loop.__name__, PlayMessage.__name__, 
            MenuMessage.__name__, VictoireMessage.__name__, InfoMessage.__name__, 
