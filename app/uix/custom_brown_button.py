c6db69cfde0bc623088c7d9edecaef348e7359d8
Matthieu <matthieu.felten22@gmail.com>
2024-01-02 16:44:27 +0100

    Change name of CustomButton to CustomBrownButton

--- app/uix/custom_brown_button.py
+++ app/uix/custom_brown_button.py
@@ -2,7 +2,7 @@ from uix.custom_press_button import CustomPressButton
 from kivy.lang import Builder
 
 
-Builder.load_file("uix/ custom_brown_button.kv")
+Builder.load_file("uix/custom_brown_button.kv")
 
 
 class CustomBrownButton(CustomPressButton):
